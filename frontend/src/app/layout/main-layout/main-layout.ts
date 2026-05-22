d774063fa754a56b370f962502758fda0322867b
Juan <juanglezmendez3@gmail.com>
2026-05-22 15:13:15 +0200

    First Layout - Login problem

--- frontend/src/app/layout/main-layout/main-layout.ts
+++ frontend/src/app/layout/main-layout/main-layout.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+// src/app/layout/main-layout/main-layout.ts
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
 export class MainLayout {
+  authService = inject(AuthService);
+  sidebarOpen = signal(false);
 
+  openSidebar() { this.sidebarOpen.set(true); }
+  closeSidebar() { this.sidebarOpen.set(false); }
+
+  logout() {
+    this.authService.logout();
+  }
 }
